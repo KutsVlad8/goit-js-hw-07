bbc283965d376d35b7d6ebea16b91367323f3d25
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-03-18 22:59:15 +0200

    update
    completed task  1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,65 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryListEl = document.querySelector(".gallery");
+
+console.log(galleryListEl);
+
+// 1. Создать галерею
+
+/* <li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</li>; */
+
+const imageForGalleryList = galleryItems
+  .map(
+    (item) => `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`
+  )
+  .join(" ");
+
+// console.log(imageForGalleryList);
+
+galleryListEl.insertAdjacentHTML("beforeend", imageForGalleryList);
+
+// 2.Реализация делегирования на ul.gallery и получение url большого изображения.
+
+galleryListEl.addEventListener("click", selectedImage);
+
+function selectedImage(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  event.preventDefault();
+
+  console.log(event.target);
+  onOpenModalImage();
+}
+
+function onOpenModalImage() {
+  basicLightbox
+    .create(
+      `
+  		<img  src="${event.target.dataset.source}">
+  	`
+    )
+    .show();
+}
